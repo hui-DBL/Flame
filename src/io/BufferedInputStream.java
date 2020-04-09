700620f1bbe442a416d865ece95d40dde3cbe7c5
hui.zhong <hui.zhong@owitho.com>
2020-04-09 22:43:05 +0800

    BufferedInputStream

--- src/io/BufferedInputStream.java
+++ src/io/BufferedInputStream.java
@@ -1,4 +1,39 @@
 package io;
 
-public class BufferedInputStream {
+import java.io.IOException;
+
+/**
+ * 功能：缓冲输入并支持mark和reset方法的功能
+ */
+public class BufferedInputStream extends FilterInputStream {
+
+    /**
+     * 8kb
+     */
+    public static final int DEFAULT_BUFFER_SIZE = 8192;
+
+    private byte[] buf;
+
+    /**
+     * 包和子类可见
+     *
+     * @param in
+     */
+    protected BufferedInputStream(InputStream in) {
+        // init
+        this(in, DEFAULT_BUFFER_SIZE);
+    }
+
+    public BufferedInputStream(InputStream in, int size) {
+        super(in);
+        if (size <= 0) {
+            throw new IllegalArgumentException("Buffer size<= 0");
+        }
+        buf = new byte[size];
+    }
+
+    @Override
+    public int read() throws IOException {
+
+    }
 }
